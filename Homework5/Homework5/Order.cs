52fa217515f0a2375a7560bca1afd735f32cd2fe
WHU_Lagueen <543776833@qq.com>
2020-03-22 14:30:57 +0800

    C#课堂练习代码

---
+++ Homework5/Homework5/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+
+    public class Order
+    {
+        public Customer customer; //客户
+        public string orderTime; //订单时间
+        public List<OrderItem> itemList; //订单明细
+        public int orderNo; //订单号
+        public Order(Customer customer1, int orderNo1, List<OrderItem> itemList1)
+        {
+            this.customer = customer1;
+            this.itemList = itemList1;
+            this.orderNo++;
+        }
+
+        public int TotalPrice()
+        {
+            int total = 0;
+            foreach (OrderItem item in itemList)
+                total += item.price * item.goodsNumber;
+            return total;
+        }
+
+        public override string ToString()
+        {
+            string items = "";
+            foreach (OrderItem item in itemList)
+                items += item;
+            return $"订单号:{orderNo};\t{items};\t商品总价:{TotalPrice()};\t购买客户:{customer.customerName}";
+        }
+
+
+    }
+
+    
+}
---
+++ Homework5/Homework5/OrderItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+   
+    public class OrderItem
+    {
+        public string goodsName { set; get; }
+        public int goodsNumber { set; get; }
+        public int price { set; get; } //订购商品列表
+        public OrderItem(string goodsname,int goodsnumber,int price1)
+        {
+            this.goodsName = goodsname;
+            this.goodsNumber = goodsnumber;
+            this.price = price1;
+        }
+        public override string ToString()
+        {
+            return $"商品名称:{goodsName}\t\t购买数量:{goodsNumber};\t\t商品单价:{price}";
+        }
+        public override bool Equals(object obj)
+        {
+            OrderItem orderItem = obj as OrderItem;
+            return orderItem != null && goodsName == orderItem.goodsName;
+        }
+    }
+}
